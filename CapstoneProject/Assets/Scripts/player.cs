16dabe167bceb6241f425fd7203e07d43819203e
Ramirgarcia <ramiro.i.garcia@outlook.com>
2023-09-09 20:47:13 -0800

    Basics of PlayerMovement, need to add camera movement too

---
+++ CapstoneProject/Assets/Scripts/player.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public float moveSpeed;
+    public bool isMoving;
+    public bool isSprinting;
+    private Vector2 input;
+    private float acceleration = 1.0001f;
+    private float topSpeed = 0.02f; //boots = 0.025 
+    private float movementTime = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        movementTime += Time.deltaTime;
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+        if (Input.GetButton("Run"))
+        {
+            topSpeed = 0.025f;
+            isSprinting = true;
+            movementTime = 0.15f;
+        } 
+        else {
+/*            if (isSprinting == true)
+            {
+                movementTime = 1.0f;
+            }*/
+            topSpeed = 0.02f;
+            isSprinting = false;
+        }
+        if (input != Vector2.zero)
+        {
+            isMoving = true;
+            if (moveSpeed < topSpeed)
+            {
+                moveSpeed = acceleration * movementTime;
+            }
+            else
+            {
+                moveSpeed = topSpeed;
+            }
+            var targetPos = transform.position;
+            targetPos.x = targetPos.x + moveSpeed * input.x;
+            targetPos.y = targetPos.y + moveSpeed * input.y;
+            transform.position = targetPos;
+            Debug.Log("Position = " + targetPos.x + " " + targetPos.y);
+            Debug.Log("timer = " + movementTime);
+        }
+        else {
+            isMoving = false;
+            movementTime = 0;
+            moveSpeed = 0;
+        }
+
+
+    }
+
+}
